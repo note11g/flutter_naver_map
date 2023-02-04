9494ffdbd54b6323549a30c1a493329651733234
note11 <note11@kakao.com>
2023-02-05 01:05:02 +0900

    add : Impl iOS NOverlayImage

--- ios/Classes/model/map/overlay/NOverlayImage.swift
+++ ios/Classes/model/map/overlay/NOverlayImage.swift
@@ -1,8 +1,48 @@
-//
-//  NOverlayImage.swift
-//  flutter_naver_map
-//
-//  Created by 김승빈 on 2023/02/02.
-//
+import NMapsMap
 
-import Foundation
+struct NOverlayImage {
+    let path: String
+    let mode: NOverlayImageMode
+
+    func applyToOverlay(setImgFunc: (NMFOverlayImage) -> Void) {
+        setImgFunc(overlayImage)
+    }
+
+    private var overlayImage: NMFOverlayImage {
+        get {
+            switch mode {
+            case .file, .temp, .widget: return makeOverlayImageWithPath()
+            case .asset: return makeOverlayImageWithAssetPath()
+            }
+        }
+    }
+
+    private func makeOverlayImageWithPath() -> NMFOverlayImage {
+        let uiImage = UIImage(contentsOfFile: path)
+        let img = NMFOverlayImage(image: uiImage!)
+        return img
+    }
+
+    private func makeOverlayImageWithAssetPath() -> NMFOverlayImage {
+        let assetPath = SwiftFlutterNaverMapPlugin.getAssets(path: path)
+        let img = NMFOverlayImage(name: assetPath)
+        return img
+    }
+
+    func toDict() -> Dictionary<String, Any> {
+        [
+            "path": path,
+            "mode": mode.rawValue
+        ]
+    }
+
+    static func fromDict(_ v: Any) -> NOverlayImage {
+        let d = asDict(v)
+        return NOverlayImage(
+                path: asString(d["path"]!),
+                mode: NOverlayImageMode(rawValue: asString(d["mode"]!))!
+        )
+    }
+
+    static let none = NOverlayImage(path: "", mode: .temp)
+}
