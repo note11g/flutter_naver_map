4ef53268b243a4592198b11ca1da436663446ec7
김승빈 <note11@kakao.com>
2023-04-14 13:48:23 +0900

    add : Overlay Example (#76)
    * add : overlay example page
    
    * add: clear each overlay function

--- example/lib/pages/examples/overlay_example.dart
+++ example/lib/pages/examples/overlay_example.dart
@@ -1,5 +1,8 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:flutter_naver_map_example/design/theme.dart';
 
 import '../../design/custom_widget.dart';
 
@@ -24,44 +27,46 @@ class _NOverlayExampleState extends State<NOverlayExample> {
   NaverMapController get mapController => widget.mapController;
 
   void attachOverlay() async {
-    final point = await mapController.getCameraPosition().then((p) => p.target);
-    final marker = NMarker(id: "1", position: point);
-    marker.setOnTapListener((m) {
-      print(m);
-      mapController.latLngToScreenLocation(m.position).then((point) {
-        print("화면상 위치 : $point");
-        addFlutterFloatingOverlay(point);
-      });
+    final cameraPosition = await mapController.getCameraPosition();
+    final overlay = NOverlayMakerUtil.makeOverlay(
+        type: willCreateOverlayType, cameraPosition: cameraPosition);
+    overlay.setOnTapListener((overlay) {
+      final latLng = cameraPosition.target;
+      mapController.latLngToScreenLocation(latLng).then(
+          (point) => addFlutterFloatingOverlay(point: point, overlay: overlay));
     });
-    mapController.addOverlay(marker);
+    mapController.addOverlay(overlay);
   }
 
   OverlayEntry? entry;
 
-  void addFlutterFloatingOverlay(NPoint point) {
-    final screenSize = MediaQuery.of(context).size;
-
+  void addFlutterFloatingOverlay({
+    required NOverlay<dynamic> overlay,
+    required NPoint point,
+  }) {
     entry = OverlayEntry(builder: (context) {
+      final xPosition = (point.x) - 28;
       return Positioned.fill(
           child: GestureDetector(
               behavior: HitTestBehavior.opaque,
-              onTap: () {
-                removeFlutterFloatingOverlay();
-              },
+              onTap: () => removeFlutterFloatingOverlay(),
               child: Stack(children: [
                 Positioned(
-                    left: (point.x) - 28,
-                    top: point.y,
+                    left: xPosition < 0 ? 0 : xPosition,
+                    top: point.y < 0 ? 0 : point.y,
                     child: Balloon(
-                        size: const Size(200, 400),
+                        size: const Size(200, 200),
                         padding: const EdgeInsets.all(8),
-                        child: Container(
-                          // color: Colors.grey,
-                          child: Column(children: const [
-                            Text("마커를 클릭하셨습니다."),
-                            Text("이 오버레이는 Flutter 위젯으로 생성되었습니다."),
-                          ]),
-                        )))
+                        backgroundColor: getColorTheme(context).background,
+                        child: ListView(children: [
+                          Text(overlay.toString()),
+                          SimpleButton(
+                              text: "지우기",
+                              action: () {
+                                mapController.deleteOverlay(overlay.info);
+                                removeFlutterFloatingOverlay();
+                              })
+                        ])))
               ])));
     });
     Overlay.of(context).insert(entry!);
@@ -88,7 +93,7 @@ class _NOverlayExampleState extends State<NOverlayExample> {
               value: willCreateOverlayType,
               onChanged: (v) => setState(() => willCreateOverlayType = v))),
       SimpleButton(
-          text: "${getOverlayKoreanName(willCreateOverlayType)} 생성",
+          text: "${willCreateOverlayType.koreanName} 생성",
           margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
           action: attachOverlay),
       if (!widget.isClosed)
@@ -97,8 +102,7 @@ class _NOverlayExampleState extends State<NOverlayExample> {
             child: Row(children: [
               Expanded(
                 child: SimpleButton(
-                    text:
-                        "${getOverlayKoreanName(willCreateOverlayType)}만 모두 지우기",
+                    text: "${willCreateOverlayType.koreanName}만 모두 지우기",
                     color: Colors.orange,
                     margin: EdgeInsets.zero,
                     action: () => mapController.clearOverlays(
@@ -117,17 +121,16 @@ class _NOverlayExampleState extends State<NOverlayExample> {
     ]);
   }
 
-  List<Widget> markerOptions() => [];
+  @override
+  void dispose() {
+    removeFlutterFloatingOverlay();
+    super.dispose();
+  }
+}
 
-  String get timeBasedId => "${DateTime.now().millisecondsSinceEpoch}";
-  static const mockLatLng = NLatLng(0, 0);
-  static const mockLatLngBounds =
-      NLatLngBounds(southWest: NLatLng(0, 0), northEast: NLatLng(0, 0));
-  static const assetImage =
-      NOverlayImage.fromAssetImage('assets/images/overlay.png'); // todo
-
-  String getOverlayKoreanName(NOverlayType type) {
-    switch (type) {
+extension NOverlayExtension on NOverlayType {
+  String get koreanName {
+    switch (this) {
       case NOverlayType.marker:
         return "마커";
       case NOverlayType.infoWindow:
@@ -147,13 +150,101 @@ class _NOverlayExampleState extends State<NOverlayExample> {
       case NOverlayType.arrowheadPathOverlay:
         return "경로(화살표) 오버레이";
       case NOverlayType.locationOverlay:
+        return "위치 오버레이";
+    }
+  }
+}
+
+class NOverlayMakerUtil {
+  static NAddableOverlay makeOverlay({
+    required NOverlayType type,
+    required NCameraPosition cameraPosition,
+    String? id,
+  }) {
+    final overlayId = id ?? _timeBasedId;
+
+    final point = cameraPosition.target;
+    final heartCoords = NOverlayMakerUtil.getHeartCoordinates(
+        cameraPosition.target,
+        zoomLevel: cameraPosition.zoom);
+    final pathCoords = [
+      point,
+      point.offsetByMeter(northMeter: -100, eastMeter: 100),
+      point.offsetByMeter(northMeter: -200),
+      point.offsetByMeter(northMeter: -300, eastMeter: 100),
+    ];
+    final secondPathCoords = [
+      pathCoords.last,
+      pathCoords.last.offsetByMeter(northMeter: -100, eastMeter: -100),
+      pathCoords.last.offsetByMeter(northMeter: -200),
+    ];
+
+    switch (type) {
+      case NOverlayType.marker:
+        return NMarker(id: overlayId, position: point);
+      case NOverlayType.infoWindow:
+        return NInfoWindow.onMap(
+            id: overlayId, position: point, text: '인포 윈도우');
+      case NOverlayType.circleOverlay:
+        return NCircleOverlay(
+            id: overlayId,
+            center: point,
+            color: Colors.green.withOpacity(0.3),
+            radius: 100,
+            outlineColor: Colors.greenAccent,
+            outlineWidth: 2);
+      case NOverlayType.groundOverlay:
+        final bounds = NLatLngBounds(
+            southWest: point,
+            northEast: point.offsetByMeter(northMeter: 422, eastMeter: 818));
+        print(bounds);
+        const img = NOverlayImage.fromAssetImage('assets/ground_img.png');
+        return NGroundOverlay(
+            id: overlayId, bounds: bounds, image: img, alpha: 1);
+      case NOverlayType.polygonOverlay:
+        return NPolygonOverlay(
+            id: overlayId,
+            coords: heartCoords,
+            color: Colors.redAccent.withOpacity(0.5));
+      case NOverlayType.polylineOverlay:
+        return NPolylineOverlay(
+            id: overlayId, coords: heartCoords, color: Colors.red);
+      case NOverlayType.pathOverlay:
+        return NPathOverlay(
+            id: overlayId, coords: pathCoords, color: Colors.green);
+      case NOverlayType.multipartPathOverlay:
+        return NMultipartPathOverlay(id: overlayId, paths: [
+          NMultipartPath(coords: pathCoords, color: Colors.amber),
+          NMultipartPath(coords: secondPathCoords, color: Colors.redAccent)
+        ]);
+      case NOverlayType.arrowheadPathOverlay:
+        return NArrowheadPathOverlay(
+            id: overlayId, coords: pathCoords, color: Colors.purple);
+      case NOverlayType.locationOverlay:
         throw Exception("locationOverlay is not supported");
     }
   }
 
-  @override
-  void dispose() {
-    removeFlutterFloatingOverlay();
-    super.dispose();
+  static List<NLatLng> getHeartCoordinates(NLatLng centerPoint,
+      {required double zoomLevel}) {
+    final radius = 20.0 / (1 + math.exp(0.75 * (zoomLevel - 14.5))) - 1.0;
+    final List<NLatLng> heartCoords = [];
+
+    for (double angle = 0; angle <= 2 * math.pi; angle += 0.01) {
+      final double x = radius * 16 * math.pow(math.sin(angle), 3);
+      final double y = radius *
+          (13 * math.cos(angle) -
+              5 * math.cos(2 * angle) -
+              2 * math.cos(3 * angle) -
+              math.cos(4 * angle));
+      final coord = centerPoint.offsetByMeter(northMeter: y, eastMeter: x);
+      heartCoords.add(coord);
+    }
+
+    return heartCoords..add(heartCoords.first);
   }
+
+  static String get _timeBasedId => "${DateTime.now().millisecondsSinceEpoch}";
+
+  NOverlayMakerUtil._();
 }
