32e3d62822b30b47d24872979010f5ab2bfbfe80
김노트 <note11@kakao.com>
2024-03-01 16:36:54 +0900

    Chore: Add api reference for `NOverlayCaption`, `NOverlayImage`

--- lib/src/type/map/overlay/overlay_image.dart
+++ lib/src/type/map/overlay/overlay_image.dart
@@ -1,7 +1,12 @@
 part of flutter_naver_map;
 
 // todo 1 : 재사용 식별자(Identifier) 사용
-// todo 2 : dynamic ratio support. (android platform의 방법을 찾지 못함)
+
+/// 지도에서 사용할 수 있는 이미지 객체입니다.
+///
+/// 에셋, 파일, byteArray, 위젯을 통해 생성할 수 있습니다.
+///
+/// 메모리의 효율적인 사용을 위해, 한번 생성한 객체는 되도록 재사용해주세요.
 class NOverlayImage with NMessageableWithMap {
   final String _path;
   final _NOverlayImageMode _mode;
@@ -12,19 +17,25 @@ class NOverlayImage with NMessageableWithMap {
   })  : _path = path,
         _mode = mode;
 
+  /// 이미지 에셋으로 지도에서 사용할 이미지를 생성합니다. (jpg, png supported)
   const NOverlayImage.fromAssetImage(String assetName)
       : _path = assetName,
         _mode = _NOverlayImageMode.asset;
 
+  /// 이미지 파일으로 지도에서 사용할 이미지를 생성합니다. (jpg, png supported)
   NOverlayImage.fromFile(File file)
       : _path = file.path,
         _mode = _NOverlayImageMode.file;
 
+  /// ByteArray로 지도에서 사용할 이미지를 생성합니다. (캐시를 사용합니다)
   static Future<NOverlayImage> fromByteArray(Uint8List imageBytes) async {
     final path = await ImageUtil.saveImage(imageBytes);
     return NOverlayImage._(path: path, mode: _NOverlayImageMode.temp);
   }
 
+  /// 위젯을 지도의 이미지로 생성합니다. (캐시를 사용합니다)
+  ///
+  /// 위젯을 이미지로 변환한 후 사용하므로, 인터렉션이 불가능함을 알아두세요.
   static Future<NOverlayImage> fromWidget({
     required Widget widget,
     required Size size,
