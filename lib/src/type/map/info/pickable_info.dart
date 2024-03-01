281e4f5f7f0583ef95dcc94ee7ae3cf166aedec2
김노트 <note11@kakao.com>
2024-03-01 16:56:46 +0900

    Chore: migrate NPickableInfo Abstract Class to Sealed Class (tested with 'addable overlays add & pick test')

--- lib/src/type/map/info/pickable_info.dart
+++ lib/src/type/map/info/pickable_info.dart
@@ -3,16 +3,13 @@ part of "../../../../flutter_naver_map.dart";
 /// [NaverMapController.pickAll]을 통해서 가져올 수 있는 요소들을 나타냅니다.
 ///
 /// 심볼의 정보를 나타내는 [NSymbolInfo]와 오버레이의 정보를 나타내는 [NOverlayInfo]가 구현체로 존재합니다.
-// TODO : Migrate to Sealed Class
-abstract class NPickableInfo {
+sealed class NPickableInfo {
   static NPickableInfo _fromMessageable(dynamic m) {
     final signature = m["signature"] as String;
-    if (signature == "symbol") {
-      return NSymbolInfo._fromMessageable(m);
-    } else if (signature == "overlay") {
-      return NOverlayInfo._fromMessageable(m);
-    } else {
-      throw NUnknownTypeCastException(unknownValue: m);
-    }
+    return switch (signature) {
+      "symbol" => NSymbolInfo._fromMessageable(m),
+      "overlay" => NOverlayInfo._fromMessageable(m),
+      _ => throw NUnknownTypeCastException(unknownValue: m),
+    };
   }
 }
