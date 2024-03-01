a763cd435422e17ba3362091ea6f7fa6a4c51cb3
김노트 <note11@kakao.com>
2024-03-01 16:34:02 +0900

    Chore: Add api reference for `NIndoorLevel`, `NIndoorRegion`, `NIndoorZone`, `NSelectedIndoor`

--- lib/src/type/map/indoor/indoor_zone.dart
+++ lib/src/type/map/indoor/indoor_zone.dart
@@ -1,10 +1,20 @@
 part of flutter_naver_map;
 
+/// 실내 지도에서 하나의 구역을 나타내는 객체입니다.
+///
+/// 실내 지도가 존재하는 영역을 나타내는 [NIndoorRegion]의 구성요소입니다.
+/// (여러개의 구역 = 하나의 영역)
 class NIndoorZone {
+  /// 구역마다 존재하는 고유의 ID입니다.
   final String id;
+
+  /// 기본으로 선택되는 구역의 층 인덱스를 나타냅니다.
   final int defaultLevelIndex;
+
+  /// 구역의 층들을 나타냅니다.
   final List<NIndoorLevel> levels;
 
+  /// 기본으로 선택되는 구역을 나타냅니다.
   NIndoorLevel get defaultLevel => levels[defaultLevelIndex];
 
   NIndoorZone._(this.id, this.defaultLevelIndex, this.levels);
--- lib/src/type/map/indoor/selected_indoor.dart
+++ lib/src/type/map/indoor/selected_indoor.dart
@@ -1,12 +1,18 @@
 part of flutter_naver_map;
 
+/// 하나의 실내 지도의 영역에서 선택된 구역, 층을 나타내는 객체입니다.
+///
+/// [NaverMap.onSelectedIndoorChanged]에서 사용됩니다.
 class NSelectedIndoor {
   final int levelIndex;
   final int zoneIndex;
+  /// 현재 보이는 실내 지도 영역입니다.
   final NIndoorRegion region;
 
+  /// 현재 보이는 층입니다.
   NIndoorLevel get level => zone.levels[levelIndex];
 
+  /// 현재 보이는 구역입니다.
   NIndoorZone get zone => region.zones[zoneIndex];
 
   NSelectedIndoor._(this.levelIndex, this.zoneIndex, this.region);
