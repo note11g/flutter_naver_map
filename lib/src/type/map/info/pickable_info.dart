5a3b75a0e5b2da642a05e4c893bfdffd42b33807
김노트 <note11@kakao.com>
2024-03-01 16:33:47 +0900

    Chore: Add api reference for `NOverlayInfo`, `NPickableInfo`, `NSymbolInfo`

--- lib/src/type/map/info/pickable_info.dart
+++ lib/src/type/map/info/pickable_info.dart
@@ -1,5 +1,9 @@
 part of flutter_naver_map;
 
+/// [NaverMapController.pickAll]을 통해서 가져올 수 있는 요소들을 나타냅니다.
+///
+/// 심볼의 정보를 나타내는 [NSymbolInfo]와 오버레이의 정보를 나타내는 [NOverlayInfo]가 구현체로 존재합니다.
+// TODO : Migrate to Sealed Class
 abstract class NPickableInfo {
   static NPickableInfo _fromMessageable(dynamic m) {
     final signature = m["signature"] as String;
--- lib/src/type/map/info/symbol_info.dart
+++ lib/src/type/map/info/symbol_info.dart
@@ -1,5 +1,6 @@
 part of flutter_naver_map;
 
+/// 심볼의 정보를 나타내는 객체입니다.
 class NSymbolInfo implements NPickableInfo {
   final String caption;
   final NLatLng position;
