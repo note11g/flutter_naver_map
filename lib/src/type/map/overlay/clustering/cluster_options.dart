fcb224cdbd4e015aaf74d2cdfd9c8f0b6724691a
김노트 <note11@kakao.com>
2024-05-22 13:50:08 +0900

    Change: change methodType `NaverMapClusteringOptions.animationDuration` from `NaverMapClusteringOptions.animate` flag property

--- lib/src/type/map/overlay/clustering/cluster_options.dart
+++ lib/src/type/map/overlay/clustering/cluster_options.dart
@@ -7,24 +7,25 @@ class NaverMapClusteringOptions with NMessageableWithMap {
   /// 기본 값은 [defaultClusteringZoomRange] (0 ~ 20).
   final NInclusiveRange<int> enableZoomRange;
 
-  /// 클러스터링이 이루어질 때 애니메이션을 실행할 지 여부를 나타냅니다.
+  /// 클러스터링이 이루어질 때 실행되는 애니메이션의 시간을 지정합니다.
   ///
-  /// 기본 값은 `true`.
-  final bool animate;
+  /// 기본 값은 `300ms`.
+  final Duration animationDuration;
 
   /// 클러스터링 기능이 활성화 될 줌 범위를 지정합니다.
   ///
-  /// 기본 값은 ``.
+  /// 기본 값은 `NClusterMergeStrategy()` 기본 생성자.
   final NClusterMergeStrategy mergeStrategy;
 
   /// 클러스터 마커의 속성을 지정할 수 있습니다.
   ///
-  /// 기본 값은 null이며, 이때는 기본적으로 [NaverMapClusterOptions.defaultClusterMarkerBuilder]가 실행됩니다.
+  /// 기본 값은 null이며, 이때는 기본적으로 [NaverMapClusteringOptions.defaultClusterMarkerBuilder]가 실행됩니다.
   final Function(NClusterInfo info, NClusterMarker clusterMarker)?
       clusterMarkerBuilder;
 
   const NaverMapClusteringOptions({
     this.enableZoomRange = defaultClusteringZoomRange,
+    this.animationDuration = const Duration(milliseconds: 300),
     this.mergeStrategy = const NClusterMergeStrategy(),
     this.clusterMarkerBuilder,
   });
