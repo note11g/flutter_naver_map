8287b86481468274557d8700cc0484a3078de9db
김노트 <note11@kakao.com>
2025-04-09 20:02:10 +0900

    [Chore] add api reference description on `FlutterNaverMap().init` method

--- lib/src/initializer/flutter_naver_map_initializer.dart
+++ lib/src/initializer/flutter_naver_map_initializer.dart
@@ -10,6 +10,15 @@ class FlutterNaverMap {
   /// 지도 사용 전에 호출해야 하는 초기화 메서드입니다.
   ///
   /// Naver Cloud Platform의 새로운 인증을 지원합니다.
+  ///
+  /// NCP 콘솔 좌측 사이드바의
+  ///
+  /// [Services > Application Services > Maps](https://console.ncloud.com/maps/application)에서
+  /// [Application 등록](https://console.ncloud.com/maps/application/create)을 클릭 후,
+  ///
+  /// API 선택에서 "Dynamic Map"을 체크합니다.
+  ///
+  /// 이후, 인증정보에서 Client ID 값을 확인하실 수 있습니다.
   Future<void> init({
     String? clientId,
     Function(NAuthFailedException ex)? onAuthFailed,
