4b2098a737f6e3976aeae62a405dcd7e6df4471f
김노트 <note11@kakao.com>
2025-06-06 19:39:10 +0900

    [Impl, Android] impl `NDefaultMyLocationTracker.requestLocationPermission`

--- android/src/main/kotlin/dev/note11/flutter_naver_map/flutter_naver_map/util/location/NDefaultMyLocationTracker.kt
+++ android/src/main/kotlin/dev/note11/flutter_naver_map/flutter_naver_map/util/location/NDefaultMyLocationTracker.kt
@@ -13,7 +13,7 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
 
 class NDefaultMyLocationTracker(messenger: BinaryMessenger, val activity: Activity) :
-    NDefaultMyLocationTrackerHandler {
+    NDefaultMyLocationTrackerHandler, PluginRegistry.RequestPermissionsResultListener {
     private val methodChannel = MethodChannel(messenger, "NDefaultMyLocationTracker")
     private val locationEventChannel =
         EventChannel(messenger, "NDefaultMyLocationTracker.locationStream")
@@ -21,6 +21,9 @@ class NDefaultMyLocationTracker(messenger: BinaryMessenger, val activity: Activi
         EventChannel(messenger, "NDefaultMyLocationTracker.headingStream")
     private val locationStreamHandler = NDefaultMyLocationTrackerLocationStreamHandler()
     private val headingStreamHandler = NDefaultMyLocationTrackerHeadingStreamHandler()
+
+    private var permissionHandlerCallback: ((result: NDefaultMyLocationTrackerPermissionStatus) -> Unit)? =
+        null
 
     init {
         methodChannel.setMethodCallHandler(::handle)
@@ -35,11 +38,65 @@ class NDefaultMyLocationTracker(messenger: BinaryMessenger, val activity: Activi
     }
 
     override fun requestLocationPermission(result: MethodChannel.Result) {
-        TODO("Not yet implemented")
+        val hasPermission =
+            ContextCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+
+        if (hasPermission) {
+            result.success(NDefaultMyLocationTrackerPermissionStatus.Granted.toMessageable())
+            return
+        }
+
+        permissionHandlerCallback = { permissionResult ->
+            result.success(permissionResult.toMessageable())
+        }
+
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String?>,
+        grantResults: IntArray
+    ): Boolean {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            val callback = permissionHandlerCallback ?: return false
+            val status: NDefaultMyLocationTrackerPermissionStatus =
+                when (grantResults.firstOrNull()) {
+                    PackageManager.PERMISSION_GRANTED -> NDefaultMyLocationTrackerPermissionStatus.Granted
+                    PackageManager.PERMISSION_DENIED -> {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                                activity,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            )
+                        ) {
+                            NDefaultMyLocationTrackerPermissionStatus.Denied
+                        } else {
+                            NDefaultMyLocationTrackerPermissionStatus.DeniedForever
+                        }
+                    }
+
+                    else -> NDefaultMyLocationTrackerPermissionStatus.Denied
+                }
+            callback.invoke(status)
+            return true
+        }
+        return false
     }
 
     override fun getCurrentPositionOnce(result: MethodChannel.Result) {
-        TODO("Not yet implemented")
+        // todo: impl
+        result.success(LatLng(37.5666102, 126.9783881).toMessageable()) // todo: remove code for test
+    }
+
+    companion object {
+        const val LOCATION_PERMISSION_REQUEST_CODE = 706
     }
 }
 
