490dcfe3b124540a8335f60a54442c5e34a0f397
김노트 <note11@kakao.com>
2025-08-05 01:51:32 +0900

    [Add, Test] add test `clustering marker after clear all overlays (issue #342)`

--- example/integration_test/clustering_test.dart
+++ example/integration_test/clustering_test.dart
@@ -33,4 +33,59 @@ void clusteringTests() {
       expect(tappedInfo, clusterableMarker.info);
     });
 
+    testNaverMap("clustering marker after clear all overlays (issue #342)",
+        (controller, tester) async {
+      final NCameraPosition(target: currentPosition, :zoom) =
+          controller.nowCameraPosition;
+      final fourteenMeterToLogicalPixel = controller.getMeterPerDpAtLatitude(
+          latitude: currentPosition.latitude, zoom: zoom);
+      const centerOfCurrentPosition = NPoint(150, 150);
+
+      final tenClusterableMarkers = List<NClusterableMarker>.generate(
+        10,
+        (index) => NClusterableMarker(
+          id: "test_$index",
+          position: currentPosition.offsetByMeter(
+              eastMeter: (index % 2 == 0 ? index.toDouble() : 0.0) * 20.0,
+              northMeter: (index % 2 == 1 ? index.toDouble() : 0.0) * -20.0),
+        ),
+      );
+      await controller.addOverlayAll(tenClusterableMarkers.toSet());
+      await tester.pumpAndSettleOnMap();
+
+      // Check if markers are clustered
+      final infos = await controller.pickAll(
+        centerOfCurrentPosition,
+        radius: fourteenMeterToLogicalPixel * 2,
+      );
+
+      print(infos);
+      expect(infos.firstOrNull, isA<NOverlayInfo>());
+      expect((infos.firstOrNull as NOverlayInfo?)?.type,
+          NOverlayType.clusterableMarker);
+
+      await controller.clearOverlays();
+      await tester.pumpAndSettleOnMap();
+
+      final newClusterableMarkers = tenClusterableMarkers.map(
+        (marker) => NClusterableMarker(
+          id: marker.info.id,
+          position: marker.position,
+        ),
+      );
+
+      await controller.addOverlayAll(newClusterableMarkers.toSet());
+      await tester.pumpAndSettleOnMap();
+
+      final newInfos = await controller.pickAll(
+        centerOfCurrentPosition,
+        radius: fourteenMeterToLogicalPixel * 2,
+      );
+
+      print(newInfos);
+      expect(newInfos.firstOrNull, isA<NOverlayInfo>());
+      expect((newInfos.firstOrNull as NOverlayInfo?)?.type,
+          NOverlayType.clusterableMarker);
+    });
+  });
 }
