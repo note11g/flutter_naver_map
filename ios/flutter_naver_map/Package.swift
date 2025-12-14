2c9e726dd05550aa8d2eeeab528bf2c6b9abd742
김노트 <note11@kakao.com>
2025-12-14 18:33:14 +0900

    [Update, iOS] use 3.23.0 naver map sdk version

--- ios/flutter_naver_map/Package.swift
+++ ios/flutter_naver_map/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "flutter-naver-map", targets: ["flutter_naver_map"])
     ],
     dependencies: [
-        .package(url: "https://github.com/navermaps/SPM-NMapsMap.git", from: "3.22.1"),
+        .package(url: "https://github.com/navermaps/SPM-NMapsMap.git", from: "3.23.0"),
     ],
     targets: [
         .target(
